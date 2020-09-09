1e7d10a0c518289a3719725a3de6e7dca83b1193
Amit Hazra <ahazratwt@gmail.com>
2020-09-09 21:57:06 +0530

    + active route class

--- views/parts/header.php
+++ views/parts/header.php
@@ -11,19 +11,30 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
+<?php
+function isRoute(array $routes)
+{
+    foreach ($routes as $route) {
+        if ($route === $_SERVER['REQUEST_URI']) return true;
+    }
+
+    return false;
+}
+?>
+
 <body>
 
     <nav class="navbar navbar-expand navbar-dark bg-dark">
         <a class="navbar-brand" href="/">Simple Rotuer</a>
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
             <li class="nav-item">
-                <a class="nav-link" href="/">Home</a>
+                <a class="nav-link <?php if (isRoute(["/", ""])) echo "active"; ?>" href="/">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/about">About</a>
+                <a class="nav-link <?php if (isRoute(["/about"])) echo "active"; ?>" href="/about">About</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/function">Function</a>
+                <a class="nav-link <?php if (isRoute(["/function"])) echo "active"; ?>" href="/function">Function</a>
             </li>
         </ul>
     </nav>
